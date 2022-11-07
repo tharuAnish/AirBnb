868f7023783df9eaf4b6fc429b2b29e9bd0be9d7
tharuAnish <namoanishtharu@gmail.com>
2022-11-08 02:08:26 +0530

    cards added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Card from './components/Card';
 import Hero from './components/Hero';
 import NavBar from './components/NavBar';
 
@@ -8,6 +9,30 @@ function App() {
     <div className="App">
       <NavBar/>
       <Hero/>
+      <div className='card-container'>
+        <Card
+          image='./card1.png'
+          description='Life lessons with Katie Zaferes'
+          price='136'
+          rating='5.0'
+          ratingNo='6'
+        />
+        <Card
+          image='./card2.png'
+          description='Learn wedding photography'
+          price='125'
+          rating='5.0'
+          ratingNo='30'
+        />
+        <Card
+          image='./card3.png'
+          description='Group Mountain Biking'
+          price='50'
+          rating='4.8'
+          ratingNo='2'
+        />
+       
+      </div>
     </div>
   );
 }
--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,9 +1,18 @@
-import 'card.style.css'
+import './card.style.css'
 
-function Card () {
+function Card (props) {
     return(
-        <div>
-            <h1>card</h1>
+            <div className='card wrapper'>
+            <img className='cardimg' src={props.image} alt="card-img" />
+            <div className='sub-text'>
+                <img className='star' src="star.png" alt="star" />
+                <span>{props.rating}</span>
+                <span className='light-text'>({props.ratingNo}).</span>
+                <span className='light-text'>USA</span>
+            </div>
+            <p>{props.description}</p>
+            <span>From${props.price} /&ensp;</span>
+            <span className='light-text'>person</span>
         </div>
     )
 }
