348d8f45392bdf103b6e4bca72a162c50bbd3b5a
tharuAnish <namoanishtharu@gmail.com>
2022-11-07 20:51:01 +0530

    adding initials with navbar component

---
+++ src/components/Hero/index.jsx
@@ -0,0 +1,13 @@
+import './hero.style.css'
+
+function Hero () {
+    return(
+        <div className="hero wrapper">
+            <img src="./photo-grid.png" alt="photogrid" />
+            <h2>Online Experiences</h2>
+            <p>Join unique interactive activities led by <br /> one-of-a-kind hosts—all without leaving home.</p>
+        </div>
+    )
+}
+
+export default Hero
---
+++ src/components/NavBar/index.jsx
@@ -0,0 +1,11 @@
+import './navBar.style.css'
+
+function NavBar () {
+    return(
+        <div className='nav'>
+            <img src="./airbnb-logo.png" alt="logo" />
+        </div>
+    )
+}
+
+export default NavBar
