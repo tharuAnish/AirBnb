507d6b3e58dcf9e4c521da18b9c9d6bd4efc60ce
tharuAnish <namoanishtharu@gmail.com>
2022-11-10 00:27:31 +0530

    codefixing

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
     return (
         <Card
             key={item.id}
-            item={item}
+            {...item}
         />
     )
 })
--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -3,21 +3,21 @@ import "./card.style.css";
 function Card(props) {
   return (
     <div className="card wrapper">
-      {props.item.openSpots === 0 ? (
+      {props.openSpots === 0 ? (
         <div className="badge">SOLDOUT</div>
       ) : (
         <div className="badge">AVAILABLE</div>
       )}
-      <img className="cardimg" src={props.item.coverImg} alt="card-img" />
+      <img className="cardimg" src={props.coverImg} alt="card-img" />
       <div className="sub-text">
         <img className="star" src="star.png" alt="star" />
-        <span>{props.item.stats.rating}</span>
-        <span className="light-text">({props.item.stats.reviewCount}).</span>
+        <span>{props.stats.rating}</span>
+        <span className="light-text">({props.stats.reviewCount}).</span>
         <span className="light-text">USA</span>
       </div>
-      <p className="description">{props.item.description}</p>
+      <p className="description">{props.description}</p>
       <div className="lowerText">
-        <div className="price">From${props.item.price} /&ensp;</div>
+        <div className="price">From${props.price} /&ensp;</div>
         <div className="light-text">person</div>
       </div>
     </div>
