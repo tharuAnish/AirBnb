bf727b7fa92c0f18b3a2c2d07fa7db5e30ca8d0a
tharuAnish <namoanishtharu@gmail.com>
2022-11-10 00:00:00 +0530

    local data store and styling

--- src/App.js
+++ src/App.js
@@ -2,35 +2,23 @@ import './App.css';
 import Card from './components/Card';
 import Hero from './components/Hero';
 import NavBar from './components/NavBar';
-
+import data  from './data'
 
 function App() {
+  const cards = data.map(item => {
+    return (
+        <Card
+            key={item.id}
+            item={item}
+        />
+    )
+})
   return (
     <div className="App">
       <NavBar/>
       <Hero/>
       <div className='card-container'>
-        <Card
-          image='./card1.png'
-          description='Life lessons with Katie Zaferes'
-          price='136'
-          rating='5.0'
-          ratingNo='6'
-        />
-        <Card
-          image='./card2.png'
-          description='Learn wedding photography'
-          price='125'
-          rating='5.0'
-          ratingNo='30'
-        />
-        <Card
-          image='./card3.png'
-          description='Group Mountain Biking'
-          price='50'
-          rating='4.8'
-          ratingNo='2'
-        />
+      {cards}
        
       </div>
     </div>
--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,20 +1,27 @@
-import './card.style.css'
+import "./card.style.css";
 
-function Card (props) {
-    return(
-            <div className='card wrapper'>
-            <img className='cardimg' src={props.image} alt="card-img" />
-            <div className='sub-text'>
-                <img className='star' src="star.png" alt="star" />
-                <span>{props.rating}</span>
-                <span className='light-text'>({props.ratingNo}).</span>
-                <span className='light-text'>USA</span>
-            </div>
-            <p>{props.description}</p>
-            <span>From${props.price} /&ensp;</span>
-            <span className='light-text'>person</span>
-        </div>
-    )
+function Card(props) {
+  return (
+    <div className="card wrapper">
+      {props.item.openSpots === 0 ? (
+        <div className="badge">SOLDOUT</div>
+      ) : (
+        <div className="badge">AVAILABLE</div>
+      )}
+      <img className="cardimg" src={props.item.coverImg} alt="card-img" />
+      <div className="sub-text">
+        <img className="star" src="star.png" alt="star" />
+        <span>{props.item.stats.rating}</span>
+        <span className="light-text">({props.item.stats.reviewCount}).</span>
+        <span className="light-text">USA</span>
+      </div>
+      <p className="description">{props.item.description}</p>
+      <div className="lowerText">
+        <div className="price">From${props.item.price} /&ensp;</div>
+        <div className="light-text">person</div>
+      </div>
+    </div>
+  );
 }
 
-export default Card
+export default Card;
